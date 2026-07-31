b7f3364a32ece659ebb9061b2e66653bdd9557e1
Nat! <nat@mulle-kybernetik.com>
2026-07-31 11:55:34 +0200

    release prep

--- .mulle/etc/env/environment-host-ci-prerelease.sh
+++ .mulle/etc/env/environment-host-ci-prerelease.sh
@@ -35,18 +35,6 @@ export MULLE__STORAGE_BRANCH="prerelease"
 
 
 #
-# mulle-storage branch: ${MULLE__STORAGE_BRANCH}
-#
-export MULLE__STORAGE_BRANCH="prerelease"
-
-
-#
-# mulle-container nodetype: ${MULLE__CONTAINER_NODETYPE:-tar}
-#
-export MULLE__CONTAINER_NODETYPE="git"
-
-
-#
 # mulle-container nodetype: ${MULLE__CONTAINER_NODETYPE:-tar}
 #
 export MULLE__CONTAINER_NODETYPE="git"
@@ -71,18 +59,6 @@ export MULLE__ALLOCATOR_NODETYPE="git"
 
 
 #
-# mulle-allocator nodetype: ${MULLE__ALLOCATOR_NODETYPE:-tar}
-#
-export MULLE__ALLOCATOR_NODETYPE="git"
-
-
-#
-# mulle-allocator url: ${MULLE__ALLOCATOR_URL:-https://github.com/mulle-c/mulle-allocator/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
-
-
-#
 # mulle-allocator url: ${MULLE__ALLOCATOR_URL:-https://github.com/mulle-c/mulle-allocator/archive/${MULLE_TAG}.tar.gz}
 #
 export MULLE__ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
@@ -92,18 +68,6 @@ export MULLE__ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
 # mulle-allocator branch: ${MULLE__ALLOCATOR_BRANCH}
 #
 export MULLE__ALLOCATOR_BRANCH="prerelease"
-
-
-#
-# mulle-allocator branch: ${MULLE__ALLOCATOR_BRANCH}
-#
-export MULLE__ALLOCATOR_BRANCH="prerelease"
-
-
-#
-# mulle-c11 nodetype: ${MULLE__C11_NODETYPE:-tar}
-#
-export MULLE__C11_NODETYPE="git"
 
 
 #
